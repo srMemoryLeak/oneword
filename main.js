ed4e28ef203dcfef4b6013c915ca4ae32a6943ae
Memory_Leak <108615630+srMemoryLeak@users.noreply.github.com>
2022-07-25 01:37:03 +0800

    Update main.js

--- main.js
+++ main.js
@@ -83,5 +83,5 @@ function addElement(){
       }
 
 function  toluntan(){
-    window.href = "https://www.shireyishunjian.com/main/forum.php?mod=viewthread&tid=218215"
+    window.open("https://www.shireyishunjian.com/main/forum.php?mod=viewthread&tid=218215")
 }
